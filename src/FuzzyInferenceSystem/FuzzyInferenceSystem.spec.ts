1e8f4d11eca0b2a0ca98f47db24cd28b0089114e
Craig Knott <psyck@nottingham.ac.uk>
2022-08-31 12:13:39 +0100

    Add linguistic rules to fuzzy inference systems

--- src/FuzzyInferenceSystem/FuzzyInferenceSystem.spec.ts
+++ src/FuzzyInferenceSystem/FuzzyInferenceSystem.spec.ts
@@ -27,6 +27,19 @@ const goodService = new FuzzySet('Good').generateMembershipValues({
 
 const serviceVariable = new LinguisticVariable('Service').addSet(poorService).addSet(goodService);
 
+const goodTip = new FuzzySet('Good').generateMembershipValues({
+  type: MembershipFunctionType.Gaussian,
+  parameters: {
+    center: 5,
+    standardDeviation: 1.5,
+    minValue: 0,
+    maxValue: 10,
+    step: 0.5,
+  },
+});
+
+const tipVariable = new LinguisticVariable('Tip').addSet(goodTip);
+
 describe('FuzzyInferenceSystem', () => {
   it('should able to be created with no sets', () => {
     const tipper = new FuzzyInferenceSystem('Tipper');
@@ -58,5 +71,50 @@ describe('FuzzyInferenceSystem', () => {
 
     expect(tipper.inputs).toHaveLength(1);
     expect(tipper.outputs).toHaveLength(1);
+  });
+
+  it('should be able to add a rule', () => {
+    const tipper = new FuzzyInferenceSystem('Tipper').addInput(serviceVariable).addOutput(tipVariable);
+    tipper.addRule('AND', [['Service', 'Good']], ['Tip', 'Good']);
+    expect(tipper.rules[0].prettyPrint()).toBe('IF Service IS Good THEN Tip IS Good');
+
+    tipper.addRule(
+      'AND',
+      [
+        ['Service', 'Good'],
+        ['Service', 'Good'],
+      ],
+      ['Tip', 'Good']
+    );
+    expect(tipper.rules[1].prettyPrint()).toBe('IF Service IS Good AND Service IS Good THEN Tip IS Good');
+  });
+
+  it('should throw an error if the antecedent or consequent are malformed', () => {
+    const tipper = new FuzzyInferenceSystem('Tipper').addInput(serviceVariable).addOutput(tipVariable);
+    expect(() => tipper.addRule('AND', [], ['Tip', 'Good'])).toThrowError(
+      'No antecedents (inputs) specified'
+    );
+    expect(() => tipper.addRule('AND', [['Food', 'Good', 'Service']], ['Tip', 'Good'])).toThrowError(
+      'All antecedents should have two parts: a linguistic variable name, and a fuzzy set name'
+    );
+    expect(() => tipper.addRule('AND', [['Food', 'Good']], [])).toThrowError(
+      'No consequent (output) specified'
+    );
+    expect(() => tipper.addRule('AND', [['Food', 'Good']], ['Tip'])).toThrowError(
+      'A consequent should have two parts: a linguistic variable name, and a fuzzy set name'
+    );
+  });
+
+  it('should throw an error if a rule does not contain valid variables/sets', () => {
+    const tipper = new FuzzyInferenceSystem('Tipper').addInput(serviceVariable).addOutput(tipVariable);
+    expect(() => tipper.addRule('AND', [['Service', 'Good']], ['Tipx', 'Good'])).toThrowError(
+      'Consequent cannot be created (set or variable do not exist)'
+    );
+    expect(() => tipper.addRule('AND', [['Service', 'Great']], ['Tip', 'Good'])).toThrowError(
+      'Antecedents could not be be created (at least one set or variable does not exist)'
+    );
+    expect(() => tipper.addRule('AND', [['Food', 'Good']], ['Tip', 'Good'])).toThrowError(
+      'Antecedents could not be be created (at least one set or variable does not exist)'
+    );
   });
 });
--- src/FuzzyInferenceSystem/FuzzyInferenceSystem.ts
+++ src/FuzzyInferenceSystem/FuzzyInferenceSystem.ts
@@ -1,14 +1,17 @@
 import { LinguisticVariable } from '../index';
+import { Antecedent, Consequent, LinguisticRule, LinguisticRuleOperator } from '../LinguisticRule';
 
 export class FuzzyInferenceSystem {
   name: string;
   inputs: LinguisticVariable[];
   outputs: LinguisticVariable[];
+  rules: LinguisticRule[];
 
   constructor(name: string, inputs: LinguisticVariable[] = [], outputs: LinguisticVariable[] = []) {
     this.name = name;
     this.inputs = inputs;
     this.outputs = outputs;
+    this.rules = [];
   }
 
   addVariable = (variable: LinguisticVariable, type: 'Input' | 'Output') => {
@@ -23,4 +26,64 @@ export class FuzzyInferenceSystem {
   addInput = (variable: LinguisticVariable) => this.addVariable(variable, 'Input');
 
   addOutput = (variable: LinguisticVariable) => this.addVariable(variable, 'Output');
+
+  addRule = (operator: LinguisticRuleOperator, antecedents: string[][], consequent: string[]) => {
+    if (antecedents.length === 0) {
+      throw new Error('No antecedents (inputs) specified');
+    }
+
+    if (antecedents.flat().length % 2 !== 0) {
+      throw new Error(
+        'All antecedents should have two parts: a linguistic variable name, and a fuzzy set name'
+      );
+    }
+
+    if (consequent.length === 0) {
+      throw new Error('No consequent (output) specified');
+    }
+
+    if (consequent.length !== 2) {
+      throw new Error('A consequent should have two parts: a linguistic variable name, and a fuzzy set name');
+    }
+
+    try {
+      this.checkAntecedentsAndConsequentAreValid(antecedents, consequent);
+    } catch ({ message }) {
+      throw new Error(message as string);
+    }
+
+    const rule = new LinguisticRule(
+      operator,
+      antecedents.map((antecedentParts) => ({
+        linguisticVariable: antecedentParts[0],
+        fuzzySet: antecedentParts[1],
+      })),
+      {
+        linguisticVariable: consequent[0],
+        fuzzySet: consequent[1],
+      }
+    );
+
+    this.rules.push(rule);
+    return this;
+  };
+
+  checkAntecedentsAndConsequentAreValid = (antecedents: string[][], consequent: string[]) => {
+    const allAntecedentsExist = antecedents.every((antecedent) => {
+      return (
+        this.inputs.find((input) => antecedent[0] === input.name)?.indexedFuzzySets[antecedent[1]] !==
+        undefined
+      );
+    });
+    if (!allAntecedentsExist) {
+      throw new Error('Antecedents could not be be created (at least one set or variable does not exist)');
+    }
+
+    const consequentExists =
+      this.outputs.find((output) => consequent[0] === output.name)?.indexedFuzzySets[consequent[1]] !==
+      undefined;
+    if (!consequentExists) {
+      throw new Error('Consequent cannot be created (set or variable do not exist)');
+    }
+  };
 }
