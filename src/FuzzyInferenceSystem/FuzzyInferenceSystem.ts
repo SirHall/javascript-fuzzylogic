3edddc141316ed9db8b1d437623a6a3ee0ac364d
SirHall <sirhallstein@gmail.com>
2023-04-14 22:54:02 +1000

    Add separate fuzzy solver

--- src/FuzzyInferenceSystem/FuzzyInferenceSystem.ts
+++ src/FuzzyInferenceSystem/FuzzyInferenceSystem.ts
@@ -2,44 +2,54 @@ import { DefuzzicationType } from '../defuzzify';
 import { LinguisticVariable } from '../index';
 import { LinguisticRule } from '../LinguisticRule';
 import { EditLinguisticVariableArgs, VariableType } from './types';
-import { mamdaniInference } from './utils';
+import { mamdaniInference, mamdaniInferenceSeparate } from './utils';
 
-export class FuzzyInferenceSystem {
+export class FuzzyInferenceSystem
+{
   name: string;
   inputs: LinguisticVariable[];
   outputs: LinguisticVariable[];
   rules: LinguisticRule[];
 
-  constructor(name: string, inputs: LinguisticVariable[] = [], outputs: LinguisticVariable[] = []) {
+  constructor(name: string, inputs: LinguisticVariable[] = [], outputs: LinguisticVariable[] = [])
+  {
     this.name = name;
     this.inputs = inputs;
     this.outputs = outputs;
     this.rules = [];
   }
 
-  addVariable = (variable: LinguisticVariable, type: VariableType) => {
-    if (type === 'Input') {
+  addVariable = (variable: LinguisticVariable, type: VariableType) =>
+  {
+    if (type === 'Input')
+    {
       this.inputs.push(variable);
-    } else {
+    } else
+    {
       this.outputs.push(variable);
     }
     return this;
   };
 
-  removeVariable = (name: string, type: VariableType) => {
-    if (type === 'Input') {
+  removeVariable = (name: string, type: VariableType) =>
+  {
+    if (type === 'Input')
+    {
       this.inputs = this.inputs.filter((variable) => variable.name !== name);
-    } else {
+    } else
+    {
       this.outputs = this.outputs.filter((variable) => variable.name !== name);
     }
     return this;
   };
 
-  removeInput = (name: string) => {
+  removeInput = (name: string) =>
+  {
     return this.removeVariable(name, VariableType.Input);
   };
 
-  removeOutput = (name: string) => {
+  removeOutput = (name: string) =>
+  {
     return this.removeVariable(name, VariableType.Output);
   };
 
@@ -47,14 +57,17 @@ export class FuzzyInferenceSystem {
     name: string,
     { name: newName, fuzzySets }: EditLinguisticVariableArgs,
     type: VariableType
-  ) => {
+  ) =>
+  {
     let oldVariableIndex: number;
     let oldVariable: LinguisticVariable;
 
-    if (type === VariableType.Input) {
+    if (type === VariableType.Input)
+    {
       oldVariableIndex = this.inputs.findIndex((variable) => variable.name === name);
       oldVariable = this.inputs[oldVariableIndex];
-    } else {
+    } else
+    {
       oldVariableIndex = this.outputs.findIndex((variable) => variable.name === name);
       oldVariable = this.outputs[oldVariableIndex];
     }
@@ -63,13 +76,15 @@ export class FuzzyInferenceSystem {
     const newVariableSets = fuzzySets ? fuzzySets : oldVariable?.fuzzySets || [];
     const newVariable = new LinguisticVariable(newVariableName, newVariableSets);
 
-    if (type === VariableType.Input) {
+    if (type === VariableType.Input)
+    {
       this.inputs = [
         ...this.inputs.slice(0, oldVariableIndex),
         newVariable,
         ...this.inputs.slice(oldVariableIndex + 1, this.inputs.length),
       ];
-    } else {
+    } else
+    {
       this.outputs = [
         ...this.outputs.slice(0, oldVariableIndex),
         newVariable,
@@ -80,64 +95,81 @@ export class FuzzyInferenceSystem {
     return this;
   };
 
-  editInput = (name: string, { name: newName, fuzzySets }: EditLinguisticVariableArgs) => {
-    if (newName && newName !== name && this.inputs.find((variable) => variable.name === newName)) {
+  editInput = (name: string, { name: newName, fuzzySets }: EditLinguisticVariableArgs) =>
+  {
+    if (newName && newName !== name && this.inputs.find((variable) => variable.name === newName))
+    {
       throw new Error('An input with that name already exists');
     }
     return this.editVariable(name, { name: newName, fuzzySets }, VariableType.Input);
   };
 
-  editOutput = (name: string, { name: newName, fuzzySets }: EditLinguisticVariableArgs) => {
-    if (newName && newName !== name && this.inputs.find((variable) => variable.name === newName)) {
+  editOutput = (name: string, { name: newName, fuzzySets }: EditLinguisticVariableArgs) =>
+  {
+    if (newName && newName !== name && this.inputs.find((variable) => variable.name === newName))
+    {
       throw new Error('An output with that name already exists');
     }
     return this.editVariable(name, { name: newName, fuzzySets }, VariableType.Output);
   };
 
-  addInput = (newVariable: LinguisticVariable) => {
-    if (this.inputs.find((variable) => variable.name === newVariable.name)) {
+  addInput = (newVariable: LinguisticVariable) =>
+  {
+    if (this.inputs.find((variable) => variable.name === newVariable.name))
+    {
       throw new Error('An input with that name already exists');
     }
     return this.addVariable(newVariable, VariableType.Input);
   };
 
-  addOutput = (newVariable: LinguisticVariable) => {
-    if (this.outputs.find((variable) => variable.name === newVariable.name)) {
+  addOutput = (newVariable: LinguisticVariable) =>
+  {
+    if (this.outputs.find((variable) => variable.name === newVariable.name))
+    {
       throw new Error('An output with that name already exists');
     }
     return this.addVariable(newVariable, VariableType.Output);
   };
 
-  addRule = (rule: string) => {
+  addRule = (rule: string) =>
+  {
     const operator = rule.match('AND') ? 'AND' : 'OR';
     const parts = rule.split(/IF|IS|THEN|AND|OR| /).filter(Boolean);
 
-    if (parts.length % 2 !== 0) {
+    if (parts.length % 2 !== 0)
+    {
       throw new Error('Rule string is malformed');
     }
 
     const antecedents: string[][] = [];
     const consequent: string[] = [];
-    for (let i = 0; i < parts.length; i += 2) {
-      if (i === parts.length - 2) {
+    for (let i = 0; i < parts.length; i += 2)
+    {
+      if (i === parts.length - 2)
+      {
         consequent.push(parts[i]);
         consequent.push(parts[i + 1]);
-      } else {
+      } else
+      {
         antecedents.push([parts[i], parts[i + 1]]);
       }
     }
 
-    if (antecedents.length === 0) {
+    if (antecedents.length === 0)
+    {
       throw new Error('No antecedents (inputs) specified');
     }
 
-    if (consequent.length === 0) {
+    if (consequent.length === 0)
+    {
       throw new Error('No consequent (output) specified');
     }
 
-    try {
+    try
+    {
       this.checkAntecedentsAndConsequentAreValid(antecedents, consequent);
-    } catch ({ message }) {
+    } catch ({ message })
+    {
       throw new Error(message as string);
     }
 
@@ -157,48 +189,63 @@ export class FuzzyInferenceSystem {
     return this;
   };
 
-  checkAntecedentsAndConsequentAreValid = (antecedents: string[][], consequent: string[]) => {
-    const allAntecedentsExist = antecedents.every((antecedent) => {
+  checkAntecedentsAndConsequentAreValid = (antecedents: string[][], consequent: string[]) =>
+  {
+    const allAntecedentsExist = antecedents.every((antecedent) =>
+    {
       return (
         this.inputs.find((input) => antecedent[0] === input.name)?.indexedFuzzySets[antecedent[1]] !==
         undefined
       );
     });
-    if (!allAntecedentsExist) {
+    if (!allAntecedentsExist)
+    {
       throw new Error('Antecedents could not be be created (at least one set or variable does not exist)');
     }
 
     const consequentExists =
       this.outputs.find((output) => consequent[0] === output.name)?.indexedFuzzySets[consequent[1]] !==
       undefined;
-    if (!consequentExists) {
+    if (!consequentExists)
+    {
       throw new Error(
         `Consequent cannot be created (variable ${consequent[0]} or set ${consequent[1]} do not exist)`
       );
     }
   };
 
-  solve = (type: 'Mamdani', args: Record<string, number>, defuzzicationMethod: DefuzzicationType): number => {
+  solve = (type: 'Mamdani', args: Record<string, number>, defuzzicationMethod: DefuzzicationType): number =>
+  {
     const allInputsHaveArg = this.inputs.every((input) => args[input.name] !== undefined);
-    if (!allInputsHaveArg) {
+    if (!allInputsHaveArg)
+    {
       throw new Error('Not all input variables have an argument provided');
     }
 
-    if (this.inputs.length === 0) {
+    if (this.inputs.length === 0)
+    {
       throw new Error('Cannot solve: No inputs defined');
     }
 
-    if (this.outputs.length === 0) {
+    if (this.outputs.length === 0)
+    {
       throw new Error('Cannot solve: No outputs defined');
     }
 
-    if (this.rules.length === 0) {
+    if (this.rules.length === 0)
+    {
       throw new Error('Cannot solve: No rules defined');
     }
 
-    if (type === 'Mamdani') {
+    if (type === 'Mamdani')
+    {
       return mamdaniInference(this.inputs, this.outputs, this.rules, args, defuzzicationMethod);
     }
     throw new Error('Unknown defuzzification method specified');
+  };
+
+  solveSeparate = (args: Record<string, number>, defuzzicationMethod: DefuzzicationType): Record<string, number> =>
+  {
+    return mamdaniInferenceSeparate(this.inputs, this.outputs, this.rules, args, defuzzicationMethod);
   };
 }
--- src/FuzzyInferenceSystem/utils.ts
+++ src/FuzzyInferenceSystem/utils.ts
@@ -9,10 +9,12 @@ export const mamdaniInference = (
   rules: LinguisticRule[],
   args: Record<string, number>,
   defuzzicationMethod: DefuzzicationType
-): number => {
+): number =>
+{
   // Create an indexed object for all the inputs and outputs
-  const variables: Record<string, { [key: string]: FuzzySet }> = [...inputs, ...outputs].reduce(
-    (acc, variable) => {
+  const variables: Record<string, { [key: string]: FuzzySet; }> = [...inputs, ...outputs].reduce(
+    (acc, variable) =>
+    {
       return {
         ...acc,
         [variable.name]: variable.indexedFuzzySets,
@@ -22,21 +24,27 @@ export const mamdaniInference = (
   );
 
   // Check all args have corresponding variables
-  Object.keys(args).forEach((variableName) => {
-    if (variables[variableName] === undefined) {
+  Object.keys(args).forEach((variableName) =>
+  {
+    if (variables[variableName] === undefined)
+    {
       throw new Error(`Argument ${variableName} does not relate to any variable in the system`);
     }
   });
 
-  const outputSets = rules.map((rule) => {
+  const outputSets = rules.map((rule) =>
+  {
     // Get all membership values for the antecedents
-    const inputMembershipValues: number[] = rule.antecedents.map(({ linguisticVariable, fuzzySet }) => {
+    const inputMembershipValues: number[] = rule.antecedents.map(({ linguisticVariable, fuzzySet }) =>
+    {
       const variable = variables[linguisticVariable][fuzzySet];
-      if (variable === undefined) {
+      if (variable === undefined)
+      {
         throw new Error(`Unable to find variable ${linguisticVariable} with set ${fuzzySet}`);
       }
       const value = variable.getMembership(args[linguisticVariable]);
-      if (value === undefined) {
+      if (value === undefined)
+      {
         throw new Error(
           `Unable to get find get membership for arg ${linguisticVariable} from ${linguisticVariable} with set ${fuzzySet}`
         );
@@ -51,7 +59,8 @@ export const mamdaniInference = (
     // Create a new FuzzyValue[] that uses the minimum value from the singleInputValue or
     // the membership of each xValue in the output set
     return variables[rule.consequent.linguisticVariable][rule.consequent.fuzzySet].values.map(
-      ({ value, membership }) => {
+      ({ value, membership }) =>
+      {
         return {
           value,
           membership: Math.min(singleInputValue, membership),
@@ -65,9 +74,11 @@ export const mamdaniInference = (
   return defuzzify(defuzzicationMethod, singleOutputSet);
 };
 
-export const combineSetsWithMaximum = (sets: FuzzyValue[][]): FuzzyValue[] => {
+export const combineSetsWithMaximum = (sets: FuzzyValue[][]): FuzzyValue[] =>
+{
   const firstSet = sets[0];
-  const minimumOfSets = firstSet.map(({ value }, i) => {
+  const minimumOfSets = firstSet.map(({ value }, i) =>
+  {
     const membershipForAllSets = sets.map((set) => set[i].membership);
     return {
       value,
@@ -75,4 +86,76 @@ export const combineSetsWithMaximum = (sets: FuzzyValue[][]): FuzzyValue[] => {
     };
   });
   return minimumOfSets;
+};
+
+export const mamdaniInferenceSeparate = (
+  inputs: LinguisticVariable[],
+  outputs: LinguisticVariable[],
+  rules: LinguisticRule[],
+  args: Record<string, number>,
+  defuzzicationMethod: DefuzzicationType
+): Record<string, number> =>
+{
+  // Create an indexed object for all the inputs and outputs
+  const variables: Record<string, { [key: string]: FuzzySet; }> = [...inputs, ...outputs].reduce(
+    (acc, variable) =>
+    {
+      return {
+        ...acc,
+        [variable.name]: variable.indexedFuzzySets,
+      };
+    },
+    {}
+  );
+
+  // Check all args have corresponding variables
+  Object.keys(args).forEach((variableName) =>
+  {
+    if (variables[variableName] === undefined)
+    {
+      throw new Error(`Argument ${variableName} does not relate to any variable in the system`);
+    }
+  });
+
+  return Object.fromEntries(
+    rules.map((rule) =>
+    {
+      // Get all membership values for the antecedents
+      const inputMembershipValues: number[] = rule.antecedents.map(({ linguisticVariable, fuzzySet }) =>
+      {
+        const variable = variables[linguisticVariable][fuzzySet];
+        if (variable === undefined)
+        {
+          throw new Error(`Unable to find variable ${linguisticVariable} with set ${fuzzySet}`);
+        }
+        const value = variable.getMembership(args[linguisticVariable]);
+        if (value === undefined)
+        {
+          throw new Error(
+            `Unable to get find get membership for arg ${linguisticVariable} from ${linguisticVariable} with set ${fuzzySet}`
+          );
+        }
+        return value;
+      });
+
+      // Use the operator (AND => Min, OR => Max) to get a single value
+      const singleInputValue =
+        rule.operator === 'AND' ? Math.min(...inputMembershipValues) : Math.max(...inputMembershipValues);
+
+      // Create a new FuzzyValue[] that uses the minimum value from the singleInputValue or
+      // the membership of each xValue in the output set
+      return [
+        defuzzify(defuzzicationMethod,
+          variables[rule.consequent.linguisticVariable][rule.consequent.fuzzySet].values.map(
+            ({ value, membership }) =>
+            {
+              return {
+                value,
+                membership: Math.min(singleInputValue, membership),
+              };
+            }
+          )
+        )
+      ];
+    }));
 };
